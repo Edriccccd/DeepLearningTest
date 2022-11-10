42d0db7398ac905d2f8f3f26e7487c43227ea38a
Edriccccd <2019210495@mail.hfut.edu.cn>
2022-11-11 00:18:20 +0800

    yyyyyy

--- Datasets.py
+++ Datasets.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import os.path
+import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,10 +8,10 @@ from torch.optim import Adam
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose,ToTensor,Normalize
 
-BATCH_SIZE = 256
-
+BATCH_SIZE = 128
+TEST_BATCH_SIZE = 1000
 #1.Datas
-def get_dataloader(train = True):
+def get_dataloader(train = True,batch_size = BATCH_SIZE):
     transform_fn = Compose([
         ToTensor(),
         Normalize(mean=(0.1307,),std=(0.3081,))
@@ -35,10 +37,14 @@ class MnistModel(nn.Module):
         x = F.relu(x)
         #4输出层
         out = self.fc2(x)
-        return F.log_softmax(out)
+        return F.log_softmax(out,dim = -1)
 
 model = MnistModel()
 optimizer = Adam(model.parameters(),lr = 0.001)
+
+if os.path.exists("./model/model.pk1"):
+    model.load_state_dict(torch.load("./model/model.pk1"))
+    optimizer.load_state_dict(torch.load("./model/optimizer.pk1"))
 
 def train(epoch):
     data_loader = get_dataloader()
@@ -48,10 +54,34 @@ def train(epoch):
         loss = F.nll_loss(output,traget)#得到损失函数
         loss.backward()    #反向传播
         optimizer.step()
+        if idx%10 == 0:
+            print(epoch,idx,loss.item())
+
         if idx%100 == 0:
-            print(loss.item())
+            torch.save(model.state_dict(),"./model/model.pk1")
+            torch.save(optimizer.state_dict(), "./model/optimizer.pk1")
+
+def test():
+    loss_list = []
+    acc_list = []
+    test_dataloader = get_dataloader(train = False,batch_size=TEST_BATCH_SIZE)
+    for idx,(input,traget) in enumerate(test_dataloader):
+        with torch.no_grad():
+            output =  model(input)
+            cur_loss = F.nll_loss(output,traget)
+            loss_list.append(cur_loss)
+            pred = output.max(dim = -1)[-1]
+            cur_acc = pred.eq(traget).float().mean()
+            acc_list.append(cur_acc)
+    print("平均准确率，平均损失：",np.mean(acc_list),np.mean(loss_list))
+
 
 
 if __name__ == '__main__':
-    for i in range(3):
-        train(i)
+    # for i in range(3):
+    #     train(i)
+    # loader =  get_dataloader(train = False)
+    # for input,lable in loader:
+    #     print(lable.size())
+    #     break
+    test()
